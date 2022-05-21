6f8da8f0206cb76ab7d2ffd11ea151aec841896d
Stanford Lin <StanfordLin@users.noreply.github.com>
2022-05-21 18:26:58 +0200

    Playing around  with CSS

--- src/index.js
+++ src/index.js
@@ -8,3 +8,19 @@ document.getElementById("app").innerHTML = `
   <a href="https://parceljs.org" target="_blank" rel="noopener noreferrer">here</a>.
 </div>
 `;
+
+// Selection of HTML objects
+const burger = document.querySelector(".burger i");
+const nav = document.querySelector(".nav");
+
+// Defining a function
+function toggleNav() {
+  burger.classList.toggle("fa-bars");
+  burger.classList.toggle("fa-times");
+  nav.classList.toggle("nav-active");
+}
+
+// Calling the function after click event occurs
+burger.addEventListener("click", function () {
+  toggleNav();
+});
